eacb87dc8e20435edd845d79a154fb26f3007dab
DESKTOP-OA5C7QI\lenovo <canedojonathan8@gmail.com>
2024-11-05 23:59:13 -0600

    Integracion del Proyecto a Git

---
+++ GestorEmpleados/Controllers/ProvedorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiWebAPI.Data;
+using MiWebAPI.Models;
+
+namespace MiWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProvedorController : ControllerBase
+    {
+        private readonly ProvedorData _ProvedorData;
+        public ProvedorController(ProvedorData ProvedorData)
+        {
+            _ProvedorData = ProvedorData;
+        }
+
+        [HttpPost]
+        [Route("GetProvedor")]
+        public async Task<IActionResult> Lista([FromBody] string filtro)
+        {
+            List<Provedor> Lista = await _ProvedorData.GetProvedor(filtro);
+            return StatusCode(StatusCodes.Status200OK, Lista);
+        }
+        [HttpPost]
+        [Route("AddProvedor")]
+        public async Task<IActionResult> AddProvedor([FromBody] Provedor objeto)
+        {
+
+            var respuesta = await _ProvedorData.AddProvedor(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpPost]
+        [Route("UpdateProvedor")]
+        public async Task<IActionResult> UpdateEmpleado([FromBody] Provedor objeto)
+        {
+
+            var respuesta = await _ProvedorData.UpdateProvedor(objeto);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+        [HttpPost]
+        [Route("DeleteProvedor")]
+        public async Task<IActionResult> DeleteProvedor([FromBody] int Id)
+        {
+
+            var respuesta = await _ProvedorData.DeleteProvedor(Id);
+            return StatusCode(StatusCodes.Status200OK, new { isSuccess = respuesta });
+        }
+
+    }
+}
---
+++ GestorEmpleados/Data/ProvedoresData.cs
@@ -0,0 +1,156 @@
+﻿using GestorEmpleados.API.Models;
+using Microsoft.EntityFrameworkCore;
+using MiWebAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MiWebAPI.Data
+{
+    public class ProvedorData
+    {
+
+        private readonly string conexion;
+        public ProvedorData(IConfiguration configuration)
+        {
+            conexion = configuration.GetConnectionString("CadenaSQL")!;
+        }
+
+
+        /// <summary>
+        /// Consulta lista de empleados
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<Provedor>> GetProvedor(string filtro)
+        {
+            List<Provedor> lista = new List<Provedor>();
+
+            using (var con = new SqlConnection(conexion))
+            {
+                await con.OpenAsync();
+                SqlCommand cmd = new SqlCommand("SP_SELECIONARPROVEDOR", con);
+                cmd.Parameters.AddWithValue("@filtro", filtro);
+                cmd.CommandType = CommandType.StoredProcedure;
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        lista.Add(new Provedor
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Nombre = reader["NombreCompleto"].ToString(),
+                            Direccion = reader["Direccion"].ToString(),
+                            Telefono = Convert.ToInt32(reader["Telefono"]),
+                            Edad = Convert.ToInt32(reader["Edad"]),
+                            Empresa = reader["Empresa"].ToString(),
+
+
+
+
+                        });
+                    }
+                }
+            }
+            return lista;
+        }
+
+        /// <summary>
+        /// Agrega un empleado
+        /// </summary>
+        /// <param name="objeto"></param>
+        /// <returns></returns>
+        public async Task<RespuestaDB> AddProvedor(Provedor objeto)
+        {
+            var resultado = new RespuestaDB();
+
+            using (var con = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("SP_AgregarProvedores", con);
+                cmd.Parameters.AddWithValue("@Nombre", objeto.Nombre);
+                cmd.Parameters.AddWithValue("@ApellidoPaterno", objeto.ApellidoPaterno);
+                cmd.Parameters.AddWithValue("@ApellidoMaterno", objeto.ApellidoMaterno);
+                cmd.Parameters.AddWithValue("@Direccion", objeto.Direccion);
+                cmd.Parameters.AddWithValue("@Telefono", objeto.Telefono);
+                cmd.Parameters.AddWithValue("@Edad", objeto.Edad);
+                cmd.Parameters.AddWithValue("@Empresa", objeto.Empresa);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+
+                        resultado.TipoError = Convert.ToInt32(reader["TipoError"]);
+                        resultado.Mensaje = reader["Mensaje"].ToString();
+
+
+                    }
+                }
+
+            }
+            return resultado;
+        }
+
+        public async Task<RespuestaDB> UpdateProvedor(Provedor objeto)
+        {
+            var resultado = new RespuestaDB();
+
+            using (var con = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("SP_ActualizarProvedor", con);
+                cmd.Parameters.AddWithValue("@id", objeto.Id);
+                cmd.Parameters.AddWithValue("@Nombre", objeto.Nombre);
+                cmd.Parameters.AddWithValue("@ApellidoPaterno", objeto.ApellidoPaterno);
+                cmd.Parameters.AddWithValue("@ApellidoMaterno", objeto.ApellidoMaterno);
+                cmd.Parameters.AddWithValue("@Direccion", objeto.Direccion);
+                cmd.Parameters.AddWithValue("@Telefono", objeto.Telefono);
+                cmd.Parameters.AddWithValue("@Edad", objeto.Edad);
+                cmd.Parameters.AddWithValue("@Empresa", objeto.Empresa);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+
+                        resultado.TipoError = Convert.ToInt32(reader["TipoError"]);
+                        resultado.Mensaje = reader["Mensaje"].ToString();
+
+
+                    }
+                }
+
+            }
+            return resultado;
+        }
+
+        public async Task<RespuestaDB> DeleteProvedor(int Id)
+        {
+            var resultado = new RespuestaDB();
+
+            using (var con = new SqlConnection(conexion))
+            {
+
+                SqlCommand cmd = new SqlCommand("SP_EliminarProvedor", con);
+                cmd.Parameters.AddWithValue("@id", Id);
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                using (var reader = await cmd.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+
+                        resultado.TipoError = Convert.ToInt32(reader["TipoError"]);
+                        resultado.Mensaje = reader["Mensaje"].ToString();
+
+
+                    }
+                }
+
+            }
+            return resultado;
+        }
+
+    }
+}
